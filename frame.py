55b9db9b11036fd439f12d34122405034bdc6ad1
rohullah <rohullah.khorami98@gmail.com>
2024-04-16 21:29:11 +0200

    added the class autoclicker

--- frame.py
+++ frame.py
@@ -1,14 +1,30 @@
 import tkinter as tk
 
 
+class autoClicker:
+    def __init__(self) -> None:
+        self.root = tk.Tk()
+        self.width = 0
+        self.height = 0
+    def setSize(self,width:int, height:int):
+        self.width = width
+        self.height = height
+        self.root.geometry(f"{self.width}x{self.height}")
+    def setFrame(self):
+        self.main_frame = tk.Frame(self.root)
+        self.main_frame.pack()
+    def closeButton(self):
+        self.close_button = tk.Button(self.root,text="Close", command=self.root.destroy)
+        self.close_button.place(x=20,y=20)
+    def run(self):
+        self.setSize(self.width,self.height)
+        self.setFrame()
+        self.closeButton()
+        self.root.mainloop()
 
-root = tk.Tk()
-root.geometry()
-main_frame = tk.Frame(root)
-
-close_button = tk.Button(root,text="Close",command=root.destroy)
-close_button.place(x=20,y=20)
-main_frame.pack()
 
 
-root.mainloop()
+if __name__=="__main__":
+    clicks = autoClicker()
+    clicks.setSize(40,40)
+    clicks.run()
